f1339f5d13320fb7ff1f9a1c30ecc936a8fdcfc6
ReneLaverdure <renelaverdure@gmail.com>
2023-01-04 20:59:00 +1100

    remove consoles

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const nav = document.querySelector('.navbar__links');
 
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('navbar--visible');
-    console.log('yes')
+
 })
 
-console.log('lol')
